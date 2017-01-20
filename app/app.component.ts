3ad2118609b4196845b66e445d223c12d298a776
Eric Robert Brewer <eric.r.brewer@gmail.com>
2017-01-20 13:34:46 -0800

    First issue.
    Attempting to pass property of ‘item’ as 2nd parameter of (loaded)
    event.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "my-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    allItems: Array<Item>;
+    currentItems: Array<Item> = [];
+
+    ngOnInit() {
+        this.allItems = [];
+        for (let i = 0; i < 8; i++) {
+            let item = { id: i };
+            this.allItems.push(item);
+        }
+
+        this.mixItems();
+    }
+
+    onMixTapped(event) {
+        this.mixItems();
+    }
+
+    /** Make a random subset of `allItems` */
+    mixItems() {
+        this.currentItems = [];
+        for (let i = 0; i < this.allItems.length; i++) {
+            let random = AppComponent.nextInt(2) + 1;
+            if (random === 1) {
+                this.currentItems.push(this.allItems[i]);
+            }
+        }
+    }
+
+    public onItemLabelLoaded(event, id: number) {
+        console.log("Loaded item: "+id);
+        // call a function here using 'id'
+    }
+
+    static nextInt(range: number): number {
+        return Math.floor(Math.random() * range);
+    }
+}
+
+class Item {
+    id: number;
 }
